11ce28d0660b421ba2aca3c1b3d2508192f73570
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2018-12-18 10:09:27 +0700

    update html for number maniulation

---
+++ array/assets/js/array.js
@@ -0,0 +1,3 @@
+console.log("learn-array");
+var bilangan_ganjil = [1, 3, 5, 7, "..."];
+var bilangan_genap = [2, 4, 6, 8, "..."];
---
+++ number-manipulation/assets/js/number-manipulation.js
@@ -0,0 +1,22 @@
+console.log("Manipulation Number");
+var number_a = 10;
+var number_b = 20;
+var number_c = 30;
+
+console.log(`Result_plus = "number_a" + "number_b" + "number_c" =`);
+result_plus = number_a + number_b + number_c;
+
+console.log(`Result_minus = "number_a" - "number_b" - "number_c" =`);
+result_minus = number_a - number_b - number_c;
+
+console.log(`Result_plus_minus = "number_a" + "number_b" - "number_c" =`);
+result_plus_minus = number_a + number_b - number_c;
+
+console.log(`Result_minus_plus = "number_a" - "number_b" + "number_c" =`);
+result_minus_plus = number_a - number_b + number_c;
+
+console.log(`Result_multiple = "number_a" * "number_b" * "number_c" =`);
+result_multiple = number_a * number_b * number_c;
+
+console.log(`Result_divided_by = "number_a" / "number_b" / "number_c" =`);
+result_divided_by = number_a / number_b / number_c;
